cb2953c8dc8301ea2aec367f8f4120a8cf74aec5
Awesome Kalin <47223207+AwesomeKalin@users.noreply.github.com>
2024-08-22 18:34:30 +0000

    Don't hardcode cloudflare tunnels

--- nginx.sh
+++ nginx.sh
@@ -6,5 +6,9 @@ echo "[Docker] Starting PHP-FPM"
 php-fpm$PHP_VERSION -c /home/container/php/php.ini --fpm-config /home/container/php/php-fpm.conf --daemonize > /dev/null 2>&1
 
 echo "[Docker] Starting NGINX"
+# Setup Cloudflared
+echo "[Docker] Starting Cloudflare Tunnels"
+cloudflared tunnel login
+cloudflared tunnel run nginxPhp
 echo "[Docker] Services successfully launched"
 nginx -c /home/container/nginx/nginx.conf -p /home/container
